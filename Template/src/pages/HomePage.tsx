3813f40cbd48b843906ef59e561c0728886a6433
Abdulrahman-Ayman-Farid <149811994+Abdulrahman-Ayman-Farid@users.noreply.github.com>
2025-05-13 23:46:05 +0300

    Renamed Models

--- Template/src/pages/HomePage.tsx
+++ Template/src/pages/HomePage.tsx
@@ -6,8 +6,8 @@ const HomePage: React.FC = () => {
     <div className="max-w-7xl mx-auto">
       <div className="text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
         <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-          <span className="block">Machine Learning</span>
-          <span className="block text-indigo-600">Web Application</span>
+          <span className="block">AiForge</span>
+          <span className="block text-indigo-600">Deployment Service</span>
         </h1>
         <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
           A platform for integrating and using machine learning models through a simple web interface.
